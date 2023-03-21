b9c1f115ab33339d64907f843080e0c482ada292
trungngvu <vutrungnghiahn@gmail.com>
2023-03-21 20:38:38 +0700

    init

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useRef } from "react";
+import { StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
+import * as Notifications from "expo-notifications";
 
 export default function App() {
+  const webViewRef = useRef(null);
+
+  const handleWebViewLoad = () => {
+    // Execute JavaScript code in the WebView context to get the window object
+    webViewRef.current.injectJavaScript(`
+    const originalNotification = window.Notification;
+    window.Notification = function(title, options) {
+      window.ReactNativeWebView.postMessage(JSON.stringify({
+        title: title,
+        body: options.body,
+        data: options.data,
+      }));
+      return new originalNotification(title, options);
+    };
+  `);
+  };
+
+  const handleWebViewMessage = (event) => {
+    // Handle incoming messages from the WebView
+    // const message = JSON.parse(event.nativeEvent.data);
+    console.log(event.nativeEvent.data);
+    // if (message.type === "WINDOW_OBJECT") {
+    //   const windowObject = message.value;
+    //   // Use the window object in your React Native code
+    //   console.log("Window object:", windowObject);
+    // }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <WebView
+        ref={webViewRef}
+        source={{
+          uri: "https://www.bennish.net/web-notifications.html",
+        }}
+        style={{ marginTop: 40 }}
+        onLoad={handleWebViewLoad}
+        onMessage={handleWebViewMessage}
+      />
+      <StatusBar />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: 5,
+  },
+  input: {
+    borderColor: "blue",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 2,
   },
 });
